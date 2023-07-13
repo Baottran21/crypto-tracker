9e7563995449f86991dd4e5840cf45b61cd00e89
Bao Tran <Baottran21@outlook.com>
2023-07-13 18:19:48 -0500

    Working through the Server JS

--- public/user-seed.sql
+++
@@ -1,3 +0,0 @@
-\c crypto
-
-INSERT INTO users (firstName, lastName, owned_coins) VALUES ('Bao', 'Tran', '{1,2,3}');
--- public/user_coin-migration.sql
+++
@@ -1,9 +0,0 @@
-\c crypto
-
-DROP TABLE IF EXISTS user_coins CASCADE;
-
-CREATE TABLE user_coins (
-    users_id INT REFERENCES users(users_id),
-    coins_id INT REFERENCES coins(coins_id),
-    PRIMARY KEY (users_id, coins_id)
-);
