f6ca05b2f7b5ce3ed192dcce60577ebef8a6d503
Bao Tran <Baottran21@outlook.com>
2023-07-14 15:54:14 -0500

    Got the Functionality for GET ALL

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
+/* eslint-disable no-undef */
 //IMPORTS
+import express from 'express';
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+//MIDDLEWARE
+const app = express();
+app.use(express.json());
+dotenv.config();
+const databaseURL = process.env.DATABASE_URL;
+const { Pool } = pkg;
+const pool = new Pool({
+  connectionString: databaseURL,
+});
+
+// console.log(pool); //Pool is working
+
+app.get('/users', async (_, res) => {
+  try {
+    const results = await pool.query(`SELECT * FROM users;`);
+    res
+      .status(200)
+      .setHeader('Content-Type', 'application/json')
+      .send(results.rows);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .setHeader('Content-Type', 'text/plain')
+      .send('INTERNAL SERVER ERROR');
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on Port ${process.env.PORT}`);
+});
