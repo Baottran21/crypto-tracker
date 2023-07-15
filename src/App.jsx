22d61358ae3d7484cc34837e3f04ba680947a5c1
Bao Tran <Baottran21@outlook.com>
2023-07-14 19:09:05 -0500

    Working on URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 /* eslint-disable no-undef */
 import { useState, useEffect } from 'react';
-
+import axios from 'axios';
 export const App = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
-      const res = await fetch(`http://localhost:8000/users`);
-      const data = await res.json();
-      console.log(data); //Returned An Array
-      setUsers(data);
+      const res = await axios.get(`http://localhost:8000/users`);
+      console.log(res); //Returned An Object
+      setUsers(res.data);
     };
 
     getData();
